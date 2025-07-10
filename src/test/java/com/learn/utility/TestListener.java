e08465b3ac71e041d12f38a48264d65be77efe6f
Nru707 <nsdanu707@gmail.com>
2025-07-10 13:16:41 +0530

    "commit"

--- src/test/java/com/learn/utility/TestListener.java
+++ src/test/java/com/learn/utility/TestListener.java
@@ -13,75 +13,75 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class TestListener implements ITestListener {
 
-	private ExtentReports reports;
-	private ExtentTest test;
+	ExtentSparkReporter htmlReporter;
+	ExtentReports reports;
+	ExtentTest test;
 
 	public void configureReport() {
-		ExtentSparkReporter htmlReporter = new ExtentSparkReporter("ExtentListenerReportDemo.html");
+		htmlReporter = new ExtentSparkReporter("ExtentListenerReportDemo.html");
 		reports = new ExtentReports();
 		reports.attachReporter(htmlReporter);
 
+		// add system information/environments info to reports
 		reports.setSystemInfo("Machine", "RaviPc");
 		reports.setSystemInfo("OS", "Windows11");
 
 		htmlReporter.config().setDocumentTitle("Extent Listener Report Demo");
 		htmlReporter.config().setReportName("This is my first Report");
 		htmlReporter.config().setTheme(Theme.DARK);
+
 	}
 
-	@Override
-	public void onStart(ITestContext context) {
-		configureReport();
-		System.out.println("Test Suite Started.");
-	}
-
-	@Override
-	public void onFinish(ITestContext context) {
-		System.out.println("Test Suite Finished.");
-		reports.flush();
-	}
-
-	@Override
 	public void onTestStart(ITestResult result) {
-		test = reports.createTest(result.getName());
+
 	}
 
-	@Override
 	public void onTestSuccess(ITestResult result) {
-		System.out.println("Test Passed: " + result.getName());
-		test.pass(MarkupHelper.createLabel("Test Passed: " + result.getName(), ExtentColor.GREEN));
+		System.out.println("Name of the test method successfully excuted " + result.getName());
+		test = reports.createTest(result.getName());
+		test.log(Status.PASS,
+				MarkupHelper.createLabel("Name of the skip test case is: " + result.getName(), ExtentColor.GREEN));
 	}
 
-	@Override
 	public void onTestFailure(ITestResult result) {
-		System.out.println("Test Failed: " + result.getName());
-		test.fail(MarkupHelper.createLabel("Test Failed: " + result.getName(), ExtentColor.RED));
+		System.out.println("Name of test method failed:" + result.getName());
+		test = reports.createTest(result.getName());
+		test.log(Status.FAIL,
+				MarkupHelper.createLabel("Name of the skip test case is: " + result.getName(), ExtentColor.RED));
 
-		String screenshotPath = System.getProperty("user.dir") + "/Screenshots/" + result.getName() + ".png";
-		File file = new File(screenshotPath);
+		String screenShotPath = System.getProperty("user.dir") + "\\Screenshots\\" + result.getName() + ".png";
 
-		if (file.exists()) {
-			try {
-				test.fail("Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
-			} catch (Exception e) {
-				test.warning("Screenshot could not be attached.");
-			}
+		File screenShotFile = new File(screenShotPath);
+
+		if (screenShotFile.exists()) {
+			test.fail("Captured Screenshot is below:" + test.addScreenCaptureFromPath(screenShotPath));
 		}
 	}
 
-	@Override
 	public void onTestSkipped(ITestResult result) {
-		System.out.println("Test Skipped: " + result.getName());
-		test.skip(MarkupHelper.createLabel("Test Skipped: " + result.getName(), ExtentColor.YELLOW));
+		System.out.println("Name of test method skipped " + result.getName());
+		test = reports.createTest(result.getName());
+		test.log(Status.SKIP,
+				MarkupHelper.createLabel("Name of the skip test case is: " + result.getName(), ExtentColor.YELLOW));
 	}
 
-	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// Optional to implement
+
 	}
 
-	@Override
 	public void onTestFailedWithTimeout(ITestResult result) {
 		onTestFailure(result);
 	}
+
+	public void onStart(ITestContext context) {
+		configureReport();
+		System.out.println("On start method invoked.....");
+	}
+
+	public void onFinish(ITestContext context) {
+		System.out.println("On Finished method invoked.....");
+		reports.flush();// it is mandatory to call flush method to ensure information is written ti the
+						// started reporter.
+	}
+
 }
